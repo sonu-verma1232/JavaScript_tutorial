9a0762e27ff5de6b4ce18d13b0c7e5dbac84662f
Sonu Verma <sonuverma282003@gmail.com>
2025-11-13 11:11:56 -0500

    Seventh commit

---
+++ followersCount/followers_count.js
@@ -0,0 +1,21 @@
+let count = 0;
+
+
+function displayCount(){
+    document.getElementById('countDisplay').innerHTML=count;
+}
+
+function checkCountValue(){
+    if(count == 10){
+        alert("Your instagram post gained 10 followers! Congratulations!");
+    }
+    else if(count == 20){
+        alert("Your instagram post gained 20 followers! Keep it up!");
+    }
+}
+
+function increaseCount() {
+    count++;  // Increment  the count by 1
+    displayCount();  // Display the count
+    checkCountValue();  // Check count value and display messages
+}
