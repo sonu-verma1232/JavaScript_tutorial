ea9aecce7e26ad5bbb8a3bc92968df19266db020
Sonu Verma <sonuverma282003@gamail.com>
2025-11-14 06:25:40 -0500

    tenth commit

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict- $22.99', 'Oatmeal- $21.99', 'Frittata- $15'];
+const mainCourseMenu = ['Steak- $15', 'Pasta- $20', 'Burger- $25', 'Salmon- $30'];
+const dessertMenu = ['Cake- $', 'Ice Cream- $25', 'Pudding- $20', 'Fruit Salad- $15'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item,index) => `<p>Item ${index+1}: ${item}</p>`).join('');
 document.getElementById("breakfastMenuItems").innerHTML = breakfastMenuItemsHTML;
