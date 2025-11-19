33bafee02c3a59fafac34858d495a83609a730d0
Sonu Verma <sonuverma282003@gmail.com>
2025-11-19 05:42:31 -0500

    don

---
+++ speedAnalysis/speed_analysis.js
@@ -0,0 +1,41 @@
+let testText = "The quick brown fox jumps over the lazy dog.";
+let startTime, endTime;
+
+function startTest(){
+    document.getElementById("inputText").value = testText;
+
+    let userInput = document.getElementById("userInput");
+    userInput.value = "";
+    userInput.readOnly = false;
+    userInput.focus();
+
+    document.getElementById('output').innerHTML = "";
+
+    startTime = new Date().getTime();
+}
+
+function endTest(){
+    endTime = new Date().getTime();
+
+    document.getElementById("userInput").readOnly = true;
+
+    var timeElapsed = (endTime - startTime)/1000;  // in seconds
+
+    var userTypedText = document.getElementById("userInput").value;
+
+    var typeWords = userTypedText.split(/\s+/).filter(function (word){
+        return word !== "";
+    }).length;
+
+    var wpm = 0;
+
+    if(timeElapsed !== 0 && !isNaN(typeWords)){
+        wpm = Math.round((typeWords/timeElapsed)*60);
+    }
+
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = "<h2>Typing Test Results: </h2>" + "<p>Words Typed: "
+    + typeWords + "</p>" + "<p>Time Elapsed: "+timeElapsed.toFixed(2)+ "seconds</p>"
+    +"<p>Words Per Minute (WPM): "+wpm +"</p>";
+
+}
